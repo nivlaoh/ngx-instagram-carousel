7bee3ba2e77d3ed04aa1e910d6c8209dbe178a3f
Alvin Ho <kaicheng.ho@sc.com>
2018-11-06 21:47:02 +0800

    Upgrade angular and added config for yanting Upgrade swiper plugin, allow multiple hashtags Removed deprecated angular modules, unused variables. Fixed autoplay not running in swiper.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
-  hashtag: 'onlywongforfera',
-  title: 'Shanjing x Ferarina\'s Wedding',
-  dateOfWedding: [24, 3, 2018],
+  hashtags: ['weitingforyou221218', '白米煮成熟范'],
+  title: 'Weiming x Yanting\'s Wedding',
+  dateOfWedding: [22, 12, 2018],
   baseUrl: 'http://sentinel-wood.hokaicheng.com:3000',
-  bufferBefore: 4
+  bufferBefore: 4,
+  showDescription: false,
+  slideTiming: 6000,
+  refreshTiming: 10000
 };
